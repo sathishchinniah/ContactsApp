762201540d87e0e09eabcd25f8d2c37388340b89
Sathish Kumar <sathish73ec@gmail.com>
2019-05-27 12:39:50 +0530

    Initial Commit.Project created with swift language.

---
+++ ContactsApp/ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by SATHISH on 5/27/19.
+//  Copyright © 2019 sathish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
